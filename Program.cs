11411e71499f659d6430aeb338b4b4b05b9bc92f
Beatriz-Claus <61596748+Beatriz-Claus@users.noreply.github.com>
2020-07-16 19:25:46 -0300

    Aula 34 MVC - Finalizada

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Aula34MVC.Controllers;
+
+namespace Aula34MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           ProdutoController produto = new ProdutoController();
+         
+           produto.Listar();
+           produto.Filtrar("199");
+        }
+    }
+}   
